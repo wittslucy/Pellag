c5970c1c2198ebefff2814e07b7d95e44c7c602a
Paula Banks <bpaula92@gmail.com>
2019-04-14 15:13:10 +0100

    Updated pages to include new User Model and Controller methods

--- views/include/navigation.php
+++ views/include/navigation.php
@@ -1,25 +1,32 @@
 <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
             <li class="nav-item">
-                <a class="nav-link <?php if ($CURRENT_PAGE === 'home') { ?> active <?php }?>" href="?controller=Pages&action=home">HOME</a>
+                <a class="nav-link <?php if ($CURRENT_PAGE === 'home') { ?> active <?php } ?>"
+                   href="?controller=Pages&action=home">HOME</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link <?php if ($CURRENT_PAGE === 'readAll') { ?> active <?php }?>" href="?controller=Post&action=readAll">ALL POSTS</a>
+                <a class="nav-link <?php if ($CURRENT_PAGE === 'readAll') { ?> active <?php } ?>"
+                   href="?controller=Post&action=readAll">ALL POSTS</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link <?php if ($CURRENT_PAGE === 'create') { ?> active <?php }?>" href="?controller=Post&action=create">CREATE NEW POST</a>
+                <a class="nav-link <?php if ($CURRENT_PAGE === 'create') { ?> active <?php } ?>"
+                   href="?controller=Post&action=create">CREATE NEW POST</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link <?php if ($CURRENT_PAGE === 'login') { ?> active <?php }?>" href="?controller=Pages&action=logIn">LOGIN</a>
+                <a class="nav-link <?php if ($CURRENT_PAGE === 'logIn') { ?> active <?php } ?>"
+                   href="?controller=User&action=logIn">LOGIN</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link <?php if ($CURRENT_PAGE === 'registerUser') { ?> active <?php }?>" href="?controller=Pages&action=registerUser">REGISTER</a>
+                <a class="nav-link <?php if ($CURRENT_PAGE === 'registerUser') { ?> active <?php } ?>"
+                   href="?controller=User&action=registerUser">REGISTER</a>
             </li>
+
 
         </ul>
     </div>
