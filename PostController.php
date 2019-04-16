e4807a43719a01655428ac1e488dfda520419f5a
EmmaEdgecombe <47867967+EmmaEdgecombe@users.noreply.github.com>
2019-04-16 18:14:05 +0100

    Add files via upload
    create post update

---
+++ PostController.php
@@ -0,0 +1,72 @@
+<?php
+
+    class PostController
+    {
+        public function readAll()
+        {
+            // Store all the posts in a variable
+            $context['allPosts'] = Post::all();
+            return render('views/posts/readAll.php', $context);
+        }
+
+        public function read()
+        {
+            // we expect a url of form ?controller=posts&action=show&id=x
+            // without an id we just redirect to the error page as we need the
+            // post id to find it in the database
+            if (isset($_GET['post_id'])) {
+                try {
+                    // we use the given id to get the correct post
+                    $context['individualPost'] = Post::find($_GET['post_id']);
+                    return render('views/posts/read.php', $context);
+
+                } catch (Exception $ex) {
+                    return call('Pages', 'error');
+                }
+            }
+            return call('Pages', 'error');
+        }
+
+        public function create()
+        {
+            // we expect a url of form ?controller=posts&action=create
+            // if it's a GET request display a blank form for creating a new product
+            // else it's a POST so add to the database and redirect to readAll action
+
+            if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+                return render('views/posts/create.php');
+            }
+            Post::add();
+            $context['allPosts'] = Post::all();
+            return render('views/posts/readAll.php', $context); 
+        }
+//
+//        public function update()
+//        {
+//            if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+//
+//                // we use the given id to get the correct product
+//                if (isset($_GET['id'])) {
+//                    $individualProduct = Post::find($_GET['id']);
+//                    require_once 'views/posts/update.php';
+//                }
+//                call('pages', 'error');
+//
+//            } else {
+//                // create an id
+//                $id = $_GET['id'];
+//                Post::update($id);
+//                $allProducts = Post::all();
+//                require_once 'views/posts/readAll.php';
+//            }
+//
+//        }
+//
+//        public function delete()
+//        {
+//            Post::remove($_GET['id']);
+//            $allProducts = Post::all();
+//            require_once 'views/posts/readAll.php';
+//        }
+
+    }
