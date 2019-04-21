3ead013ed222d67947a5016431ee8ee378920359
Paula Banks <bpaula92@gmail.com>
2019-04-21 16:17:17 +0100

    Updated templates, navigation and css to create the homepage

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,57 +1,193 @@
-<?php ?>
-
-
-<div class="form-container">
-    <div class="card-deck">
-
-        <div class="card">
-            <img class="card-img-top" src="photo_Gillian.jpg" alt="A photo of Gillian">
-            <div class="card-body">
-                <h5 class="card-title">Gillian</h5>
-                <p class="card-text"> blah blah blah about me!</p>
+<div class="container">
+    <div class="row home-page-row">
+        <div class="item">
+            <div class="card">
+                <div class="card-header">
+                    RECENT POSTS
+                </div>
+                <div class="card-body">
+                    <h5>NOTE: Requires SQL to generate a list of 10 most recent posts</h5>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Cras justo odio</li>
+                        <li class="list-group-item">Dapibus ac facilisis in</li>
+                        <li class="list-group-item">Morbi leo risus</li>
+                        <li class="list-group-item">Porta ac consectetur ac</li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                    <p></p>
+                    <a href="index.php?controller=Post&action=readAll" class="btn btn-primary">ALL POSTS</a>
+                </div>
             </div>
         </div>
-
-        <div class="card">
-            <img class="card-img-top" src="../images/Sushi.jpeg" alt="A photo of Alex">
-            <div class="card-body">
-                <h5 class="card-title">Alex</h5>
-                <p class="card-text">Alex can dance!</p>
+        <div class="item">
+            <div class="card">
+                <div class="card-header">
+                    NEW USERS
+                </div>
+                <div class="card-body">
+                    <h5>NOTE: Requires SQL to generate a list of 10 most recently registered users</h5>
+                    <p>Maybe a number of posts created and take the user to the page with posts sorted by the new
+                        author</p>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Cras justo odio</li>
+                        <li class="list-group-item">Dapibus ac facilisis in</li>
+                        <li class="list-group-item">Morbi leo risus</li>
+                        <li class="list-group-item">Porta ac consectetur ac</li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                </div>
             </div>
         </div>
-
-        <div class="card">
-            <img class="card-img-top" src="../images/photo_Paula.jpg" alt="A photo of Paula">
-            <div class="card-body">
-                <h5 class="card-title">Paula</h5>
-                <p class="card-text">Paula makes awesome cinnamon swirls!</p>
-            </div>
-        </div>    
-
-
-        <div class="card">
-            <img class="card-img-top" src="" alt="A photo of Louise">
-            <div class="card-body">
-                <h5 class="card-title">Louise</h5>
-                <p class="card-text"> Louise likes music, la la la</p>
-            </div>
-        </div>
-
-        <div class="card">
-            <img class="card-img-top" src="" alt="A photo of Emma">
-            <div class="card-body">
-                <h5 class="card-title">Emma</h5>
-                <p class="card-text">Emma is going to Iceland, and she laughs at people falling over!</p>
-            </div>
-        </div>
-
-        <div class="card">
-            <img class="card-img-top" src="" alt="A photo of Lucy">
-            <div class="card-body">
-                <h5 class="card-title">Lucy</h5>
-                <p class="card-text">Hey Lucy, give me a useful fact about yourself...</p>
+        <div class="item">
+            <div class="card">
+                <div class="card-header">
+                    JOIN US
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">JOIN THE CREATORS</h5>
+                    <p class="card-text">Follow the ling below to register to our blog</p>
+                    <a href="index.php?controller=User&action=registerUser" class="btn btn-primary">REGISTER</a>
+                </div>
             </div>
         </div>
     </div>
 </div>
+
+<div class="card card-team">
+    <div class="card-header">
+        MEET THE TEAM
+    </div>
+
+    <div class="card-body">
+        <div id="meet-the-team-carousel" class="carousel slide" data-ride="carousel" data-interval="5000">
+
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="wrapper-team">
+                        <div class="wrapper-team-image">
+                            <img class="img-responsive" width="100px" height="100px" src="views/images/KopX.jpeg"
+                                 alt="First slide">
+                        </div>
+                        <div class="wrapper-team-description">
+                            <div class="team-description-title">
+                                <h1>Hello</h1>
+                            </div>
+                            <div class="team-description-text">
+                                <p>It's awesome to write some stuff</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="carousel-item">
+                    <div class="wrapper-team">
+                        <div class="wrapper-team-image">
+                            <img class="img-responsive" width="100px" height="100px" src="views/images/KopX.jpeg"
+                                 alt="Second slide">
+                        </div>
+                        <div class="wrapper-team-description">
+                            <div class="team-description-title">
+                                <h1>Hello</h1>
+                            </div>
+                            <div class="team-description-text">
+                                <p>It's awesome to write some stuff</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="wrapper-team">
+                        <div class="wrapper-team-image">
+                            <img class="img-responsive" width="100px" height="100px" src="views/images/KopX.jpeg"
+                                 alt="Third slide">
+                        </div>
+                        <div class="wrapper-team-description">
+                            <div class="team-description-title">
+                                <h1>Hello</h1>
+                            </div>
+                            <div class="team-description-text">
+                                <p>It's awesome to write some stuff</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="wrapper-team">
+                        <div class="wrapper-team-image">
+                            <img class="img-responsive" width="100px" height="100px" src="views/images/KopX.jpeg"
+                                 alt="Third slide">
+                        </div>
+                        <div class="wrapper-team-description">
+                            <div class="team-description-title">
+                                <h1>Hello</h1>
+                            </div>
+                            <div class="team-description-text">
+                                <p>It's awesome to write some stuff</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="wrapper-team">
+                        <div class="wrapper-team-image">
+                            <img class="img-responsive" width="100px" height="100px" src="views/images/KopX.jpeg"
+                                 alt="Fourth slide">
+                        </div>
+                        <div class="wrapper-team-description">
+                            <div class="team-description-title">
+                                <h1>Hello</h1>
+                            </div>
+                            <div class="team-description-text">
+                                <p>It's awesome to write some stuff</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="wrapper-team">
+                        <div class="wrapper-team-image">
+                            <img class="img-responsive" width="100px" height="100px" src="views/images/KopX.jpeg"
+                                 alt="Fifth slide">
+                        </div>
+                        <div class="wrapper-team-description">
+                            <div class="team-description-title">
+                                <h1>Hello</h1>
+                            </div>
+                            <div class="team-description-text">
+                                <p>It's awesome to write some stuff</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="wrapper-team">
+                        <div class="wrapper-team-image">
+                            <img class="img-responsive" width="100px" height="100px" src="views/images/KopX.jpeg"
+                                 alt="Sixth slide">
+                        </div>
+                        <div class="wrapper-team-description">
+                            <div class="team-description-title">
+                                <h1>Hello</h1>
+                            </div>
+                            <div class="team-description-text">
+                                <p>It's awesome to write some stuff</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+</div>
+
+
+
 
