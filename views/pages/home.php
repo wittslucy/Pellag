c26333e5c3dda0c7fdcdebdb99afd7f207f1d6d3
Paula Banks <bpaula92@gmail.com>
2019-04-28 07:40:43 +0100

    Another missing button css

--- views/pages/home.php
+++ views/pages/home.php
@@ -18,7 +18,7 @@
                                 <blockquote class="author-bio">You have no posts yet... </blockquote>
                             </div>
                             <div class="button-center">
-                                <a href="index.php?controller=Post&action=create" class="btn-custom btn-center">Let's get started!</a>
+                                <a href="index.php?controller=Post&action=create" class="btn-custom btn btn-center">Let's get started!</a>
                             </div>
                         <? endif ?>
                     </ul>
